d2577de2753e7c22ae918f904b694b33d34d67d1
Ihor Chubov <ihor.chub@gmail.com>
2022-10-15 21:38:21 +0300

    task#1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function changeBodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let changeColorInterval = null;
+
+buttonStart.disabled = false;
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', onStart);
+buttonStop.addEventListener('click', onStop);
+
+function onStart() {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  changeBodyColor();
+  changeColorInterval = setInterval(changeBodyColor, 1000);
+}
+
+function onStop() {
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+  clearInterval(changeColorInterval);
+}
